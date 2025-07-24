7f6dd3d4a7588ad7d55333be5f02f44dfb5bdef9
pkdev <ppkock@gmail.com>
2025-07-24 11:31:52 +0200

    Documented gallows and word array, and added randomWord method with placeholder logic

--- Hangman.java
+++ Hangman.java
@@ -1,78 +1,108 @@
+import java.util.Random;
+
 public class Hangman {
 
+    /**
+    * An array of animal names used as potential target words in the game.
+    * One word is randomly selected from this list at the start of each game.
+    */
     public static String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
-    "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer",
-    "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat",
-    "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose",
-    "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", 
-    "python", "rabbit", "ram", "rat", "raven","rhino", "salmon", "seal",
-    "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan",
-    "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf",
-    "wombat", "zebra"};
+        "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer",
+        "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat",
+        "goose", "hawk", "lion", "lizard", "llama", "mole", "monkey", "moose",
+        "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", 
+        "python", "rabbit", "ram", "rat", "raven","rhino", "salmon", "seal",
+        "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan",
+        "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf",
+        "wombat", "zebra"};
 
+    /**
+    * An array of strings representing the Hangman gallows at each stage of incorrect guesses.
+    * Each index corresponds to the number of wrong guesses made (0 to 6).
+    */
     public static String[] gallows = {"+---+\n" +
-    "|   |\n" +
-    "    |\n" +
-    "    |\n" +
-    "    |\n" +
-    "    |\n" +
-    "=========\n",
+        "|   |\n" +
+        "    |\n" +
+        "    |\n" +
+        "    |\n" +
+        "    |\n" +
+        "=========\n",
 
-    "+---+\n" +
-    "|   |\n" +
-    "O   |\n" +
-    "    |\n" +
-    "    |\n" +
-    "    |\n" +
-    "=========\n",
+        "+---+\n" +
+        "|   |\n" +
+        "O   |\n" +
+        "    |\n" +
+        "    |\n" +
+        "    |\n" +
+        "=========\n",
 
-    "+---+\n" +
-    "|   |\n" +
-    "O   |\n" +
-    "|   |\n" +
-    "    |\n" +
-    "    |\n" +
-    "=========\n",
+        "+---+\n" +
+        "|   |\n" +
+        "O   |\n" +
+        "|   |\n" +
+        "    |\n" +
+        "    |\n" +
+        "=========\n",
 
-    " +---+\n" +
-    " |   |\n" +
-    " O   |\n" +
-    "/|   |\n" +
-    "     |\n" +
-    "     |\n" +
-    " =========\n",
+        " +---+\n" +
+        " |   |\n" +
+        " O   |\n" +
+        "/|   |\n" +
+        "     |\n" +
+        "     |\n" +
+        " =========\n",
 
-    " +---+\n" +
-    " |   |\n" +
-    " O   |\n" +
-    "/|\\  |\n" + //if you were wondering, the only way to print '\' is with a trailing escape character, which also happens to be '\'
-    "     |\n" +
-    "     |\n" +
-    " =========\n",
+        " +---+\n" +
+        " |   |\n" +
+        " O   |\n" +
+        "/|\\  |\n" + // backslash requires escaping: \\
+        "     |\n" +
+        "     |\n" +
+        " =========\n",
 
-    " +---+\n" +
-    " |   |\n" +
-    " O   |\n" +
-    "/|\\  |\n" +
-    "/    |\n" +
-    "     |\n" +
-    " =========\n",
+        " +---+\n" +
+        " |   |\n" +
+        " O   |\n" +
+        "/|\\  |\n" +
+        "/    |\n" +
+        "     |\n" +
+        " =========\n",
 
-    " +---+\n" +
-    " |   |\n" +
-    " O   |\n" +
-    "/|\\  |\n" + 
-    "/ \\  |\n" +
-    "     |\n" +
-    " =========\n"};
+        " +---+\n" +
+        " |   |\n" +
+        " O   |\n" +
+        "/|\\  |\n" + 
+        "/ \\  |\n" +
+        "     |\n" +
+        " =========\n"};
+
+    /**
+     * Selects and returns a random word from the static list of animal names.
+     * This method uses java.util.Random to generate a random index.
+     *
+     * @return A randomly selected animal name from the words array.
+     */
+    public static String randomWord() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(words.length);
+        return words[randomIndex];
+    }
 
     public static void main(String[] args) {
 
+        String word = randomWord(); // The word the user needs to guess
+        char[] placeholders = new char[word.length()]; // Array to hold _ _ _ placeholders
 
+        // Fill the placeholders array with underscores
+        for (int i = 0; i < placeholders.length; i++) {
+            placeholders[i] = '_';
+        }
 
-
-
-
+        // Display the initial word state with underscores
+        System.out.print("Word: ");
+        for (char c : placeholders) {
+            System.out.print(c + " ");
+        }
+        System.out.println("\n"); // Add spacing for clarity
     }
-
 }
